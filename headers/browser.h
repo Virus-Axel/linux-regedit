9fd6936ed333e1069e6d4020bcb98ddb663488b0
lod <axel.benjaminsson@live.se>
2020-12-03 11:33:46 +0100

    Added value cell header structure

--- headers/browser.h
+++ headers/browser.h
@@ -1,19 +1,29 @@
 #ifndef BROWSER_H
 #define BROWSER_H
 #include <iostream>
+#include <vector>
 
 class KeyCell{
 private:
-	int size, nodeType;
+	int size, parentOffset, numberOfSubkeys, subkeyListOffset, numberOfValues, valueListOffset, securityIdentityOffset;
 	long int lastWriteTime;
 	std::string name;
-	char ID[2];
+	short int ID, nodeType, keyNameLength;
+public:
+};
+
+class ValueCell{
+private:
+	int size, dataLength, valueType;
+	short int ID, valueNameLength;
 public:
 };
 
 class RegistryHive{
 private:
 	std::string filename;
+	std::vector <KeyCell> keyCells;
+	std::vector <ValueCell> valueCells;
 public:
 	RegistryHive();
 	RegistryHive(const std::string &filename);

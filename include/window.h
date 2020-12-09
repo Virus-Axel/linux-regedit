f69c4f510867ebf1b34e5c9c4ea386e0b2df8ce4
lod <axel.benjaminsson@live.se>
2020-12-09 16:27:56 +0100

    Added new value viewer to gui

--- include/window.h
+++ include/window.h
@@ -9,20 +9,20 @@ class Window{
 // TODO Maybe switch to namespace later
 private:
 	std::vector<RegistryHive*> openHives;
-	GtkWidget *window, *openButton, *saveButton, *hbox, *vbox, *buttonBox, *treeView, *scrollWindow1;
-	GtkWidget *grid;
-	GtkTreeModel *treeModel;
-	GtkTreeStore *treeStore;
+	GtkWidget *window, *openButton, *saveButton, *hbox, *vbox, *buttonBox, *treeView, *listView, *scrollWindow1, *scrollWindow2;
 	void createBoxes();
 	void createTree();
+	void createList();
 	static void rescale(GtkWidget* window, Window* pointer);
 	static void fileChooser(GtkWidget* button, Window* pointer);
 	static void writeChanges();
+	static void cursorChanged(GtkWidget* parent, Window* pointer);
 public:
 	Window();
 	void resize();
 	void openFile();
 	void populateTree();
+	void displayCell();
 	~Window();
 };
 
--- source/window.cpp
+++ source/window.cpp
@@ -4,20 +4,22 @@ Window::Window(){
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "linux-regedit");
 	gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+	//gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
 	openButton = gtk_button_new_with_label("Open hive");
 	saveButton = gtk_button_new_with_label("Write changes");
 	
 	createTree();
+	createList();
 	createBoxes();
 
 	resize();
 
-	g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 	// g_signal_connect(window, "check-resize", G_CALLBACK(rescale), this);
-	g_signal_connect(openButton, "clicked", G_CALLBACK(fileChooser), this);
-	g_signal_connect(saveButton, "clicked", G_CALLBACK(writeChanges), window);
+	g_signal_connect(G_OBJECT(openButton), "clicked", G_CALLBACK(fileChooser), this);
+	g_signal_connect(G_OBJECT(saveButton), "clicked", G_CALLBACK(writeChanges), window);
+	g_signal_connect(G_OBJECT(treeView), "cursor-changed", G_CALLBACK(cursorChanged), this);
 
 	gtk_widget_show_all(window);
 	gtk_main();
@@ -34,36 +36,84 @@ void Window::createBoxes(){
 
 	hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4);
 	gtk_box_pack_start(GTK_BOX(hbox), vbox, 0, 0, 4);
+	gtk_box_pack_start(GTK_BOX(hbox), scrollWindow2, 0, 0, 4);
 
 	gtk_container_add(GTK_CONTAINER(scrollWindow1), treeView);
+	gtk_container_add(GTK_CONTAINER(scrollWindow2), listView);
 	gtk_container_add(GTK_CONTAINER(window), hbox);
 }
 
 void Window::createTree(){
-	GtkTreeIter iterator;
 	GtkCellRenderer *renderer;
-	treeStore = gtk_tree_store_new(1, G_TYPE_STRING);
+	GtkTreeModel * model;
+	GtkTreeStore *treeStore = gtk_tree_store_new(2, G_TYPE_STRING, G_TYPE_POINTER);
 	scrollWindow1 = gtk_scrolled_window_new(NULL, NULL);
-
-	/*for(int i = 0; i < 10; i++){
-	gtk_tree_store_append(treeStore, &iterator, NULL);
-	gtk_tree_store_set(treeStore, &iterator, 0, "SAM", -1);
-	gtk_tree_store_append(treeStore, &iterator, &iterator);
-	gtk_tree_store_set(treeStore, &iterator, 0, "Key", -1);
-	}*/
 
 	treeView = gtk_tree_view_new();
 
 	renderer = gtk_cell_renderer_text_new();
 	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW(treeView), -1, "Registry cells", renderer, "text", 0, NULL);
 
-	treeModel = GTK_TREE_MODEL(treeStore);
-	gtk_tree_view_set_model(GTK_TREE_VIEW(treeView), treeModel);
-	g_object_unref(treeModel);
+	model = GTK_TREE_MODEL(treeStore);
+	gtk_tree_view_set_model(GTK_TREE_VIEW(treeView), model);
+	g_object_unref(model);
+}
+
+void Window::createList(){
+	GtkCellRenderer *renderer;
+	GtkListStore *listStore = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+	scrollWindow2 = gtk_scrolled_window_new(NULL, NULL);
+
+	listView = gtk_tree_view_new();
+
+	renderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW(listView), -1, "Name", renderer, "text", 0, NULL);
+
+	renderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW(listView), -1, "Value", renderer, "text", 0, NULL);
+
+	GtkTreeModel* model = GTK_TREE_MODEL(listStore);
+	gtk_tree_view_set_model(GTK_TREE_VIEW(listView), model);
+
+	g_object_unref(model);
 }
 
 void Window::writeChanges(){
 
+}
+
+void Window::cursorChanged(GtkWidget* parent, Window *pointer){
+	pointer->displayCell();
+}
+
+void Window::displayCell(){
+	GtkTreePath *path;
+	GtkTreeIter iterator;
+
+	gtk_tree_view_get_cursor(GTK_TREE_VIEW(treeView), &path, NULL);
+	if(path == NULL)
+		return;
+
+	GtkTreeModel *treeModel = gtk_tree_view_get_model(GTK_TREE_VIEW(treeView));
+	gtk_tree_model_get_iter(GTK_TREE_MODEL(treeModel), &iterator, path);
+
+	gtk_tree_path_free(path);
+
+	void *cell;
+	char *buff;
+	
+	gtk_tree_model_get(GTK_TREE_MODEL(treeModel), &iterator, 0, &buff, 1, &cell, -1);
+	if(cell == NULL){
+		if(buff != NULL)
+			free(buff);
+		return;
+	}
+
+	if(buff != NULL)
+		free(buff);
+
+	((Cell*)cell)->print();
+	std::cout << std::endl;
 }
 
 void Window::rescale(GtkWidget* window, Window* pointer){
@@ -73,10 +123,11 @@ void Window::rescale(GtkWidget* window, Window* pointer){
 void Window::resize(){
 	gint x, y;
 	gtk_window_get_size(GTK_WINDOW(window), &x, &y);
-	gtk_widget_set_size_request(hbox, x - 5, y - 40);
+	gtk_widget_set_size_request(hbox, x, y - 40);
 	gtk_widget_set_size_request(vbox, x / 2 - 5, y - 40);
 	gtk_widget_set_size_request(buttonBox, x / 2 - 5, 40);
 	gtk_widget_set_size_request(scrollWindow1, x / 2 - 5, y - 80);
+	gtk_widget_set_size_request(scrollWindow2, x / 2 - 5, y - 80);
 };
 
 void Window::fileChooser(GtkWidget* button, Window* pointer){
@@ -98,23 +149,23 @@ void Window::openFile(){
 
 void Window::populateTree(){
 	GtkTreeIter iterator, parent;
-	treeModel = gtk_tree_view_get_model(GTK_TREE_VIEW(treeView));
+	GtkTreeModel *treeModel = gtk_tree_view_get_model(GTK_TREE_VIEW(treeView));
 	g_object_ref(treeModel);
 	gtk_tree_view_set_model(GTK_TREE_VIEW(treeView), NULL);
-	treeStore = GTK_TREE_STORE(treeModel);
+	GtkTreeStore *treeStore = GTK_TREE_STORE(treeModel);
 	for(unsigned int i = 0; i < openHives.size(); i++){
 		gtk_tree_store_append(treeStore, &parent, NULL);
-		gtk_tree_store_set(treeStore, &parent, 0, openHives[i]->getName().c_str(), -1);
+		gtk_tree_store_set(treeStore, &parent, 0, openHives[i]->getName().c_str(), 1, NULL, -1);
 		for(unsigned int j = 0; j < openHives[i]->tree.size(); j++){
 			if(openHives[i]->tree[j].get()->getType() == KEY_CELL){
 				KeyCell* keyCell = (KeyCell*) openHives[i]->tree[j].get();
 				gtk_tree_store_append(treeStore, &iterator, &parent);
-				gtk_tree_store_set(treeStore, &iterator, 0, keyCell->name.c_str(), -1);
+				gtk_tree_store_set(treeStore, &iterator, 0, keyCell->name.c_str(), 1, (void*) openHives[i]->tree[j].get(), -1);
 			}
 			else if(openHives[i]->tree[j].get()->getType() == VALUE_CELL){
 				ValueCell* valueCell = (ValueCell*) openHives[i]->tree[j].get();
 				gtk_tree_store_append(treeStore, &iterator, &parent);
-				gtk_tree_store_set(treeStore, &iterator, 0, valueCell->name.c_str(), -1);
+				gtk_tree_store_set(treeStore, &iterator, 0, "value", 1, (void*) openHives[i]->tree[j].get(), -1);
 			}
 		}
 	}
@@ -127,3 +178,4 @@ Window::~Window(){
 	for(int i = 0; i < openHives.size(); i++)
 		delete openHives[i];
 }
+

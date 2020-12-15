965301ec3cf6e7880767e5ac7b84816f40c5f02a
Albin Söderström <albin.soderstrom@hotmail.com>
2020-12-15 20:41:07 +0100

    Make tree

--- include/browser.h
+++ include/browser.h
@@ -8,44 +8,59 @@
 #include <iomanip>
 #include <memory>
 #include <cstddef>
+#include <map>
+#include <thread>
 
 #include <errno.h>
 #include <time.h>
 #include <string.h>
 
+#define HBIN 4096
+
 class Cell{
 protected:
-	int offset;
+	unsigned int offset;
 public:
 	Cell();
 	virtual int readCell(std::ifstream& stream) = 0;
 	virtual void print() = 0;
+	virtual unsigned int getOffset() = 0;
+	virtual int getSize() = 0;
 	virtual ~Cell();
 };
 
 class ValueCell : public Cell{
 private:
-	int offset, size, dataLength, valueType, dataOffset;
+	unsigned int offset;
+	int size, dataLength, valueType, dataOffset;
 	short int ID, valueNameLength;
 public:
 	ValueCell();
+	ValueCell(const std::vector<std::byte> &buffer, size_t offset);
 	int readCell(std::ifstream& stream) override;
 	void print() override;
+	unsigned int getOffset() override;
+	int getSize() override;
 	~ValueCell();
 };
 
 class KeyCell : public Cell{
 private:
+	unsigned int offset;
 	int size, parentOffset, numberOfSubkeys, subkeyListOffset, numberOfValues, valueListOffset, securityIdentifierOffset;
 	long int lastWriteTime;
 	std::string name;
 	short int ID, nodeType, keyNameLength;
-	std::vector<KeyCell> subKeys;
-	std::vector<ValueCell> values;
+	std::vector<std::unique_ptr<KeyCell>> subKeys;
+	std::vector<std::unique_ptr<ValueCell>> values;
 public:
 	KeyCell();
+	KeyCell(const std::vector<std::byte> &buffer, size_t offset);
+	void makeTree(const std::vector<std::byte> &buffer, std::map<unsigned int, std::unique_ptr<Cell>> &cellMap);
 	int readCell(std::ifstream& stream) override;
 	void print() override;
+	unsigned int getOffset() override;
+	int getSize() override;
 	~KeyCell();
 };
 
@@ -58,14 +73,18 @@ private:
 	int majorVersion;
 	int minorVersion;
 	int rootCellOffset;
+	int hbinOffset;
 	int length;
 	std::string name;
-	std::vector <std::unique_ptr<Cell>> tree;
+	std::vector <std::unique_ptr<KeyCell>> tree;
+	std::vector <std::unique_ptr<Cell>> oldTree;
 public:
 	RegistryHive();
-	RegistryHive(const std::string &filepath);
-	RegistryHive(std::vector<std::byte> &buffer);
+	RegistryHive(const std::string &filepath) : RegistryHive(readFile(filepath)) {}
+	RegistryHive(const std::vector<std::byte> &buffer);
 	std::vector<std::byte> readFile(const std::string &filepath);
+	std::vector<std::unique_ptr<Cell>> getCells(const std::vector<std::byte> &buffer);
+
 	RegistryHive* operator=(const RegistryHive&);
 	int openHive(const std::string &filename);
 	~RegistryHive();
--- source/main.cpp
+++ source/main.cpp
@@ -3,39 +3,39 @@
 
 #include "browser.h"
 
-std::vector<std::byte> readFile(const std::string &filepath);
+// std::vector<std::byte> readFile(const std::string &filepath);
 
-std::vector<std::byte> readFile(const std::string &filepath) {
-	std::ifstream ifs(filepath, std::ios::binary|std::ios::ate);
+// std::vector<std::byte> readFile(const std::string &filepath) {
+// 	std::ifstream ifs(filepath, std::ios::binary|std::ios::ate);
 
-	if(!ifs)
-		throw std::runtime_error(filepath + ": " + strerror(errno));
+// 	if(!ifs)
+// 		throw std::runtime_error(filepath + ": " + strerror(errno));
 
-	auto end = ifs.tellg();
-	ifs.seekg(0, std::ios::beg);
+// 	auto end = ifs.tellg();
+// 	ifs.seekg(0, std::ios::beg);
 
-	auto size = std::size_t(end - ifs.tellg());
+// 	auto size = std::size_t(end - ifs.tellg());
 
-	if(size == 0) // Aviod undefined behaviour
-		return {};
+// 	if(size == 0) // Aviod undefined behaviour
+// 		return {};
 	
-	std::vector<std::byte> buffer(size);
+// 	std::vector<std::byte> buffer(size);
 
-	if(!ifs.read((char*)buffer.data(), buffer.size()))
-		throw std::runtime_error(filepath + ": " + strerror(errno));
+// 	if(!ifs.read((char*)buffer.data(), buffer.size()))
+// 		throw std::runtime_error(filepath + ": " + strerror(errno));
 
-	return buffer;
-}
+// 	return buffer;
+// }
 
 int main(int argc, char *argv[]) {
-    //std::vector<std::byte> hiveFile = readFile(argv[1]);
-    //std::cout << hiveFile.size() << std::endl;
+    // std::vector<std::byte> hiveFile = readFile(argv[1]);
+    // std::cout << hiveFile.size() << std::endl;
     // for (size_t i = 0; i < 4; i++)
     // {
     //     std::cout << std::hex << std::to_integer<int>(hiveFile.at(i)) << std::endl;
     // }
     
-    //RegistryHive hive = RegistryHive(hiveFile);
+    // RegistryHive hive = RegistryHive(hiveFile);
 
     RegistryHive hive = RegistryHive(argv[1]);
 }
